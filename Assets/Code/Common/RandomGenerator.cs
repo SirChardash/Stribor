5cd1971df65988616e844be82c5d3bc7443aa6cd
Chardash <nebojsa.baabic@gmail.com>
2020-02-25 22:37:35 +0100

    Fixed RandomGenerator.cs Changed initial seed from using zero date to using current date.

--- Assets/Code/Common/RandomGenerator.cs
+++ Assets/Code/Common/RandomGenerator.cs
@@ -1,10 +1,12 @@
 using System;
+using UnityEngine;
+using Random = System.Random;
 
 namespace Code.Common
 {
   public static class RandomGenerator
   {
-    private static readonly Random Random = new Random(new DateTime().Millisecond);
+    private static readonly Random Random = new Random(DateTime.Now.Millisecond);
 
     public static int RandomInt(int bound)
     {
