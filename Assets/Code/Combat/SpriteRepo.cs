cade1fbb12ac26bf90efd822c05f17af7e5872fc
Chardash <nebojsa.baabic@gmail.com>
2020-02-25 23:04:09 +0100

    Fixed SpriteRepo for when low numbers are chosen Fixed formatting of random int appended to monster_ since one-digit numbers are lead by zero.

--- Assets/Code/Combat/SpriteRepo.cs
+++ Assets/Code/Combat/SpriteRepo.cs
@@ -11,12 +11,12 @@ namespace Code.Combat
       //todo: actually implement what's promised
       var id = RandomGenerator.RandomInt(102) * 2 + 1;
       Debug.Log("random id is " + id);
-      Debug.Log("loading " + "Sprites/Characters/monster_" + id);
-      Debug.Log("loading " + "Sprites/Characters/monster_" + (id + 1));
+      Debug.Log("loading " + "Sprites/Characters/monster_" + id.ToString("00"));
+      Debug.Log("loading " + "Sprites/Characters/monster_" + (id + 1).ToString("00"));
       return new List<Sprite>
       {
-        Resources.Load<Sprite>("Sprites/Characters/monster_" + id),
-        Resources.Load<Sprite>("Sprites/Characters/monster_" + (id + 1)),
+        Resources.Load<Sprite>("Sprites/Characters/monster_" + id.ToString("00")),
+        Resources.Load<Sprite>("Sprites/Characters/monster_" + (id + 1).ToString("00")),
       };
     }
   }
